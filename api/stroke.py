63f8a26d7838bf0057d8db1df57987edc7d6939e
vibha-yganji <vibhayadavganji@gmail.com>
2024-03-20 13:41:14 -0700

    backend

---
+++ api/stroke.py
@@ -0,0 +1,100 @@
+from flask import Blueprint, jsonify, Flask, request
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
+from flask_restful import Api, Resource
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+import sqlite3
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+app = Flask(__name__)
+stroke_api = Blueprint('stroke_api', __name__, url_prefix='/api/stroke')
+api = Api(stroke_api)
+
+class Predict(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            stroke_data = pd.DataFrame(data, index=[0])
+            
+            # Preprocesssing
+            #stroke_data['gender'] = stroke_data['gender'].apply(lambda x: 1 if x == 'Male' else 0)
+            #passenger_data['alone'] = passenger_data['alone'].apply(lambda x: 1 if x == True else 0)
+
+            stroke_data.drop(['id', 'ever_married', 'work_type'], axis=1, inplace=True)
+            ## dropping all NA values in dataset
+            stroke_data.dropna(inplace=True)
+            ## convert all sex values to 0/1 (ML models can only process quantitative data)
+            stroke_data['gender'] = stroke_data['gender'].apply(lambda x: 1 if x == 'Male' else 0)
+            stroke_data['heart_disease'] = stroke_data['heart_disease'].apply(lambda x: 1 if x == 'Yes' else 0)
+            stroke_data['Residence_type'] = stroke_data['Residence_type'].apply(lambda x: 1 if x == 'Urban' else 0)
+            stroke_data['smoking_status'] = stroke_data['smoking_status'].apply(lambda x: 1 if x == 'smoked' else 0)
+
+            #onehot = enc.transform(passenger_data[['embarked']]).toarray()
+            #cols = ['embarked_' + val for val in enc.categories_[0]]
+            #passenger_data[cols] = pd.DataFrame(onehot)
+            #passenger_data.drop(['embarked'], axis=1, inplace=True)
+            
+            # Predict the survival probability for the new passenger
+            stroke_prob = gnb.predict_proba(stroke_data)[:, 1]
+            #stroke_prob = 1 - survival_prob
+
+            return {'chance of stroke': float(stroke_prob * 100)}, 200
+        except Exception as e:
+            return {'error': str(e)}, 400
+
+api.add_resource(Predict, '/predict')
+
+# Load the stroke dataset
+stroke_data = pd.read_csv('/Users/vibhaganji/stroke-ml-backend/healthcare-dataset-stroke-data.csv')
+
+# Preprocess the data
+
+stroke_data.drop(['id', 'ever_married', 'work_type'], axis=1, inplace=True)
+
+## dropping all NA values in dataset
+stroke_data.dropna(inplace=True)
+
+## convert all sex values to 0/1 (ML models can only process quantitative data)
+stroke_data['gender'] = stroke_data['gender'].apply(lambda x: 1 if x == 'Male' else 0)
+stroke_data['heart_disease'] = stroke_data['heart_disease'].apply(lambda x: 1 if x == 'Yes' else 0)
+stroke_data['Residence_type'] = stroke_data['Residence_type'].apply(lambda x: 1 if x == 'Urban' else 0)
+stroke_data['smoking_status'] = stroke_data['smoking_status'].apply(lambda x: 1 if x == 'smoked' else 0)
+
+# Encode categorical variables
+
+## onehotencode was not required for this data as there were only binary values for most variables
+## enc = OneHotEncoder(handle_unknown='ignore')
+## enc.fit(stroke_data[['embarked']])
+## onehot = enc.transform(titanic_data[['embarked']]).toarray()
+## cols = ['embarked_' + val for val in enc.categories_[0]]
+## titanic_data[cols] = pd.DataFrame(onehot)
+## titanic_data.drop(['embarked'], axis=1, inplace=True)
+##titanic_data.dropna(inplace=True)
+
+# Split the data into training and testing sets
+X = stroke_data.drop('stroke', axis=1)
+y = stroke_data['stroke']
+
+# Train a decision tree classifier
+#dt = DecisionTreeClassifier()
+#dt.fit(X_train, y_train)
+
+# Test the model
+#y_pred = dt.predict(X_test)
+
+## slightly lower accuracies
+# Split the data into features and target
+X = stroke_data.drop('stroke', axis=1)
+y = stroke_data['stroke']
+
+# Train the logistic regression model
+logreg = LogisticRegression()
+logreg.fit(X, y)
---
+++ model/strokes.py
@@ -0,0 +1,63 @@
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+import sqlite3
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+# Load the titanic dataset
+stroke_data = pd.read_csv('/Users/vibhaganji/stroke-ml-backend/healthcare-dataset-stroke-data.csv')
+
+# Preprocess the data
+
+stroke_data.drop(['id', 'ever_married', 'work_type'], axis=1, inplace=True)
+
+## dropping all NA values in dataset
+stroke_data.dropna(inplace=True)
+
+## convert all sex values to 0/1 (ML models can only process quantitative data)
+stroke_data['gender'] = stroke_data['gender'].apply(lambda x: 1 if x == 'Male' else 0)
+stroke_data['heart_disease'] = stroke_data['heart_disease'].apply(lambda x: 1 if x == 'Yes' else 0)
+stroke_data['Residence_type'] = stroke_data['Residence_type'].apply(lambda x: 1 if x == 'Urban' else 0)
+stroke_data['smoking_status'] = stroke_data['smoking_status'].apply(lambda x: 1 if x == 'smoked' else 0)
+
+# Encode categorical variables
+
+## onehotencode was not required for this data as there were only binary values for most variables
+## enc = OneHotEncoder(handle_unknown='ignore')
+## enc.fit(stroke_data[['embarked']])
+## onehot = enc.transform(titanic_data[['embarked']]).toarray()
+## cols = ['embarked_' + val for val in enc.categories_[0]]
+## titanic_data[cols] = pd.DataFrame(onehot)
+## titanic_data.drop(['embarked'], axis=1, inplace=True)
+##titanic_data.dropna(inplace=True)
+
+# Split the data into training and testing sets
+X = stroke_data.drop('stroke', axis=1)
+y = stroke_data['stroke']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Train a decision tree classifier
+#dt = DecisionTreeClassifier()
+#dt.fit(X_train, y_train)
+
+# Test the model
+#y_pred = dt.predict(X_test)
+
+## slightly lower accuracies
+X = stroke_data.drop('stroke', axis=1)
+y = stroke_data['stroke']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Train a decision tree classifier
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+
+# Test the model
+y_pred = dt.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
